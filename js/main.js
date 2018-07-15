26ace8ae2c64c0114c9f19374c57a82f4d393a7a
Fathoni Nur Muhammad <fathonimuhammad580@gmail.com>
2018-07-15 21:27:06 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+  var _url = "https://my-json-server.typicode.com/thoninumad/pwaapi/products";
+
+  var dataResults = "";
+  var catResults = "";
+
+  var categories = [];
+
+  function renderPage(data) {
+      $.each(data, function(key, items) {
+
+        _cat = items.category;
+
+        dataResults += "<div>"
+                        + "<h3>" + items.name + "</h3>"
+                        + "<p>" + _cat + "</p>"
+                        "<div>";
+
+        if($.inArray(items.category, categories) == -1) {
+          categories.push(items.category);
+          catResults += "<option value'"+ _cat +"'>" + _cat + "</option>";
+        }
+
+      })
+
+      $('#products').html(dataResults);
+      $('#cat_select').html("<option value='all'>Semua</option>" + catResults);
+  }
+
+  var networkDataReceived = false;
+
+  // fresh data from online
+  var networkUpdate = fetch(_url).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    networkDataReceived = true;
+    renderPage(data);
+  })
+
+  // return data from cache
+  caches.match(_url).then(function(response) {
+    if(!response) throw Error('no data on cache');
+    return response.json();
+  }).then(function(data) {
+    if(!networkDataReceived) {
+      renderPage(data);
+      console.log('render data from cache');
+    }
+  }).catch(function() {
+    return networkUpdate;
+  })
+
+
+  // Fungsi Filter
+  $('#cat_select').on('change', function() {
+    updateProduct($(this).val());
+  })
+
+  function updateProduct(cat) {
+
+    var dataResults = "";
+    var _newUrl = _url;
+
+    if(cat != 'all')
+      _newUrl = _url + "?category=" + cat;
+
+    $.get(_newUrl, function(data) {
+      $.each(data, function(key, items) {
+
+        _cat = items.category;
+
+        dataResults += "<div>"
+                        + "<h3>" + items.name + "</h3>"
+                        + "<p>" + _cat + "</p>"
+                        "<div>";
+      })
+
+      $('#products').html(dataResults);
+    })
+  }
+
+}) // end document ready jquery
+
+// PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
---
+++ serviceworker.js
@@ -0,0 +1,64 @@
+var CACHE_NAME = 'my-site-cache-v2';
+var urlsToCache = [
+  '/pwaapi',
+  'fallback.json',
+  'css/main.css',
+  'js/jquery.min.js',
+  'js/main.js',
+  'images/logo-ise.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('in install serviceworker... cache opened!');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+
+  var request = event.request;
+  var url = new URL(request.url);
+
+  // Pisahkan request API dan internal
+  if(url.origin === location.origin) {
+    event.respondWith(
+      caches.match(request).then(function(response) {
+        return response || fetch(request);
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.open('products-cache').then(function(cache) {
+        return fetch(request).then(function(liveResponse) {
+          cache.put(request, liveResponse.clone());
+          return liveResponse;
+        }).catch(function() {
+          return caches.match(request).then(function(response) {
+            if(response) return response;
+            return caches.match('fallback.json');
+          })
+        })
+      })
+    )
+  }
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName != CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
